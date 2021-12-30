35d1ba7004bce1bae6733857a1b7874e8258e050
xkumiyu <xkumiyu@gmail.com>
2021-12-30 21:00:06 +0900

    change the order of installation

--- install.sh
+++ install.sh
@@ -52,8 +52,8 @@ print 'Installing Homebrew...'
 if [ "$OS" = 'Linux' ]; then
   if [ -e /home/linuxbrew/.linuxbrew/bin/brew ]; then
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-  elif [ -e $HOME/.linuxbrew/bin/brew ]; then
-    eval "$($HOME/.linuxbrew/bin/brew shellenv)"
+  elif [ -e "$HOME"/.linuxbrew/bin/brew ]; then
+    eval "$("$HOME"/.linuxbrew/bin/brew shellenv)"
   fi
 fi
 if type brew > /dev/null 2>&1; then
@@ -63,8 +63,8 @@ else
   if [ "$OS" = 'Linux' ]; then
     if [ -e /home/linuxbrew/.linuxbrew/bin/brew ]; then
       eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-    elif [ -e $HOME/.linuxbrew/bin/brew ]; then
-      eval "$($HOME/.linuxbrew/bin/brew shellenv)"
+    elif [ -e "$HOME"/.linuxbrew/bin/brew ]; then
+      eval "$("$HOME"/.linuxbrew/bin/brew shellenv)"
     fi
   fi
 
@@ -73,8 +73,8 @@ else
     if [ "$OS" = 'Linux' ]; then
       if [ -e /home/linuxbrew/.linuxbrew/bin/brew ]; then
         echo "eval \"\$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)\"" >> ~/.zshrc.local
-      elif [ -e $HOME/.linuxbrew/bin/brew ]; then
-        echo "eval \"\$($HOME/.linuxbrew/bin/brew shellenv)\"" >> ~/.zshrc.local
+      elif [ -e "$HOME"/.linuxbrew/bin/brew ]; then
+        echo "eval \"\$(""$HOME""/.linuxbrew/bin/brew shellenv)\"" >> ~/.zshrc.local
       fi
     fi
   else
@@ -106,7 +106,36 @@ elif [ "$OS" = 'Linux' ]; then
 fi
 echo ''
 
-# 5. python
+# 5. shell and terminal
+change_shell=false
+if type zsh > /dev/null 2>&1 && [ "${SHELL:${#SHELL}-3}" != 'zsh' ]; then
+  print "Changing login shell to $(which zsh)..."
+  sudo chsh -s "$(which zsh)" "${USER:-$(whoami)}"
+  change_shell=true
+  echo ''
+fi
+
+if [ ! -e ~/.zplug/init.zsh ]; then
+  print "Installing zplug..."
+  git clone https://github.com/zplug/zplug.git ~/.zplug
+  zsh -ic 'zplug install'
+  echo ''
+fi
+
+if [ ! -e ~/.pure ]; then
+  print "Downloading pure..."
+  git clone https://github.com/sindresorhus/pure.git ~/.pure
+  echo ''
+fi
+
+if [ ! -e ~/.vim/colors/molokai.vim ]; then
+  print "Downloading molokai..."
+  git clone https://github.com/tomasr/molokai ~/.vim/colors/molokai
+  mv ~/.vim/colors/molokai/colors/molokai.vim ~/.vim/colors/
+  echo ''
+fi
+
+# 6. python
 if [ ! -e "$(pyenv root)"/plugins/xxenv-latest ]; then
   print 'Installing pyenv plugin...'
   git clone https://github.com/momo-lab/xxenv-latest.git "$(pyenv root)"/plugins/xxenv-latest
@@ -130,36 +159,6 @@ if [ "$(pyenv latest -P)" != "$(pyenv latest -p)" ]; then
   echo ''
 fi
 
-# 6. shell and terminal
-change_shell=false
-if type zsh > /dev/null 2>&1 && [ "${SHELL:${#SHELL}-3}" != 'zsh' ]; then
-  print "Changing login shell to $(which zsh)..."
-  sudo chsh -s "$(which zsh)" "${USER:-$(whoami)}"
-  change_shell=true
-  echo ''
-fi
-
-install_zplug=false
-if [ ! -e ~/.zplug/init.zsh ]; then
-  print "Installing zplug..."
-  git clone https://github.com/zplug/zplug.git ~/.zplug
-  install_zplug=true
-  echo ''
-fi
-
-if [ ! -e ~/.pure ]; then
-  print "Downloading pure..."
-  git clone https://github.com/sindresorhus/pure.git ~/.pure
-  echo ''
-fi
-
-if [ ! -e ~/.vim/colors/molokai.vim ]; then
-  print "Downloading molokai..."
-  git clone https://github.com/tomasr/molokai ~/.vim/colors/molokai
-  mv ~/.vim/colors/molokai/colors/molokai.vim ~/.vim/colors/
-  echo ''
-fi
-
 # 7. docker (optional)
 if [ "$OS" = 'Linux' ] && ! type docker > /dev/null 2>&1; then
   read -r -p 'Do you want to install Docker? (y/N): ' yn
@@ -176,10 +175,10 @@ if [ "$OS" = 'Linux' ] && ! type docker > /dev/null 2>&1; then
 
       # docker compose
       COMPOSE_VERSION="v2.2.2"
-      mkdir -p $HOME/.docker/cli-plugins
-      curl -fsSL -o $HOME/.docker/cli-plugins/docker-compose \
+      mkdir -p "$HOME"/.docker/cli-plugins
+      curl -fsSL -o "$HOME"/.docker/cli-plugins/docker-compose \
         "https://github.com/docker/compose/releases/download/$COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m)"
-      chmod +x $HOME/.docker/cli-plugins/docker-compose
+      chmod +x "$HOME"/.docker/cli-plugins/docker-compose
       ;;
     *)
       ;;
@@ -188,14 +187,10 @@ if [ "$OS" = 'Linux' ] && ! type docker > /dev/null 2>&1; then
 fi
 
 # finish
-if "$change_shell" || "$install_zplug"; then
+if "$change_shell"; then
   print 'Next steps:'
-  if "$change_shell"; then
-    echo -e "- Change current shell to zsh:\n    exec zsh -l"
-  fi
-  if "$install_zplug"; then
-    echo -e "- Install zplug plugins:\n    zplug install"
-  fi
+  echo -e "- Run the following command or log out and login again:"
+  echo "    su - ${USER:-$(whoami)}"
 else
   print 'The setup is complate!'
 fi
